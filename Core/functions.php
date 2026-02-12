2c1eb146f3feba687cf2405b9ff84edf1aae3e01
Otmane Boumediane <boumediane.otmane@gmail.com>
2026-02-12 21:36:11 +0100

    build a better PHP router

--- Core/functions.php
+++ Core/functions.php
@@ -17,6 +17,15 @@ function urlIs(string $value): bool
     return $uri === $value;
 }
 
+function abort(int $code = 404): void
+{
+    http_response_code($code);
+
+    require base_path("views/$code.php");
+
+    die();
+}
+
 function authorize(bool $condition, $status = Response::FORBIDDEN): void
 {
     if (!$condition) {
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,6 +10,7 @@
     <p><?= htmlspecialchars($note['body']) ?></p>
 
     <form method="post" class="mt-6">
+        <input type="hidden" name="_method" value="DELETE" />
         <input type="hidden" name="id" value="<?= $note['id'] ?>" />
         <button class="text-sm text-red-500" type="submit">Delete</button>
     </form>
