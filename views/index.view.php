fd57f3d2a2086136d3a5c33a1c610608a478b389
Otmane Boumediane <boumediane.otmane@gmail.com>
2026-02-16 23:20:20 +0000

    manage passwords like this for the remainder of your career

--- views/index.view.php
+++ views/index.view.php
@@ -3,7 +3,7 @@
 <?php require base_path('views/partials/banner.php'); ?>
 
 <main class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-    Home
+    <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?></p>
 </main>
 
 <?php require base_path('views/partials/footer.php'); ?>
